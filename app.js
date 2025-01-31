191c8d6eba45a8b88454305d25f728f1c7b4529c
Pedro Meza <mezapedro@gmail.com>
2025-01-31 17:42:09 -0300

    prueba de archivo

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//import java.util.ArrayList;
+
+let listaAmigoSecreto = [];
 
 
-if(){
-    alert("hola mundo");
+
+function agregarAmigo(){
+    
+    let amigoSecreto = document.getElementById('amigo').value; 
+   
+    if ( amigoSecreto == "" ) 
+        {
+        alert("Debe ingresar Nombre");
+        limpiarcaja();
+    }else{
+        console.log("Se ingresa " + amigoSecreto +" en Array");
+        listaAmigoSecreto.push(amigoSecreto);
+        limpiarcaja();
+        console.log(listaAmigoSecreto);
+    }
+    return amigoSecreto;
 }
+
+function limpiarcaja(){
+    document.querySelector('#amigo').value = '';
+}
+
+
+printf(amigoSecreto);
